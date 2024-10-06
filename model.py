0232d7a8a32438ed056d0adf76daba45963f9618
youdianshekong <liuyang16@stu.sau.edu.cn>
2024-10-06 15:51:07 +0000

    1

---
+++ model.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+import math
+
+class InputEmbeddings(nn.Module):
+    
+    def __init__(self,d_model:int,vocab_size:int):
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size,d_model)
+        
+    def forward(self,x):
+        return self.embedding(x) * math.sqrt(self.d_model)
+
+
+class PositionalEncoding(nn.Module):
+    
+    def __init__(self,d_modle:int,seq_len:int,dropout:float) -> None:
+        super().__init__()
+        self.d_model = d_modle
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+        
+        # Create a matrix of shape (seq_len,d_model)
+        pe = torch.zeros(seq_len,d_modle)
+        # Create a vector of shape(seq_len)
+        position = torch.arange(0, seq_len, dtype=torch.float64).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0,d_model,2).float() * (-math.log(10000.0) / d_modle))  # type: ignore
