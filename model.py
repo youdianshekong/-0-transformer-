a14993eedbbafa121d70d3535d8e9658a7eeb78f
youdianshekong <liuyang16@stu.sau.edu.cn>
2024-10-06 16:43:13 +0000

    2

--- model.py
+++ model.py
@@ -25,5 +25,16 @@ class PositionalEncoding(nn.Module):
         # Create a matrix of shape (seq_len,d_model)
         pe = torch.zeros(seq_len,d_modle)
         # Create a vector of shape(seq_len)
-        position = torch.arange(0, seq_len, dtype=torch.float64).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0,d_model,2).float() * (-math.log(10000.0) / d_modle))  # type: ignore
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0,d_model,2).float() * (-math.log(10000.0) / d_modle))  
+        # Apply the sin to even position
+        pe[:,0::2] = torch.sin(position * div_term)
+        pe[:,1::2] = torch.cos(position * div_term)
+        
+        pe = pe.unsqueeze(0) # (1, Seq_len, d_model)
+        
+        self.register_buffer('pe', pe)
+        
+    def forward(self, x):
+        x = x + (self.pe[:, :x.shape[1], :]).requires_grad(False)
+        return self.dropout(x)
