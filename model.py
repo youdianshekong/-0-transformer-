0beeafe1e31b0b765eb0a4493d515a23d23ad5d1
youdianshekong <liuyang16@stu.sau.edu.cn>
2024-10-07 08:07:12 +0000

    4

--- model.py
+++ model.py
@@ -21,17 +21,17 @@ class InputEmbeddings(nn.Module):
 
 class PositionalEncoding(nn.Module):
     
-    def __init__(self,d_modle:int,seq_len:int,dropout:float) -> None:
+    def __init__(self,d_model:int,seq_len:int,dropout:float) -> None:
         super().__init__()
-        self.d_model = d_modle
+        self.d_model = d_model
         self.seq_len = seq_len
         self.dropout = nn.Dropout(dropout)
         
         # Create a matrix of shape (seq_len,d_model)
-        pe = torch.zeros(seq_len,d_modle)
+        pe = torch.zeros(seq_len,d_model)
         # Create a vector of shape(seq_len)
         position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0,d_model,2).float() * (-math.log(10000.0) / d_modle))   # type: ignore
+        div_term = torch.exp(torch.arange(0,d_model,2).float() * (-math.log(10000.0) / d_model))   
         # Apply the sin to even position
         pe[:,0::2] = torch.sin(position * div_term)
         pe[:,1::2] = torch.cos(position * div_term)
